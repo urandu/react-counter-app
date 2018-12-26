37cc2482b71d724250980e29841bbb919a8f88c7
bildad namawa <bildadnamawa@gmail.com>
2018-12-26 23:34:01 +0300

    uplifting state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,9 +17,7 @@ class Counter extends Component {
         {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => {
-            this.handleIncrement({ id: 1 });
-          }}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -31,36 +29,18 @@ class Counter extends Component {
         >
           Delete
         </button>
-        {this.state.tags.length === 0 && "Please create new tag!"}
-        {/* {this.renderTags()} */}
       </React.Fragment>
-    );
-  }
-
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
