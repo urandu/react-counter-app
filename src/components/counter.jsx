39d2626a07e9d4959f9dafea56a8a35afc313068
bildad namawa <bildadnamawa@gmail.com>
2018-12-26 22:56:27 +0300

    removing local state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
     tags: []
   };
 
@@ -23,6 +23,13 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
         {this.state.tags.length === 0 && "Please create new tag!"}
         {/* {this.renderTags()} */}
