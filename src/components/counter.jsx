fd1651704cd62aa976d7e74cbe0a4ccdbc0eef54
bildad namawa <bildadnamawa@gmail.com>
2018-12-26 21:24:52 +0300

    updating the state

---
+++ src/components/counter.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: []
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "Please create new tag!"}
+        {/* {this.renderTags()} */}
+      </React.Fragment>
+    );
+  }
+
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+  };
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
